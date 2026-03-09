e24a049aa9d1ec05d1bb07c5b290f77310d53835
thoweber <karazon@gmail.com>
2026-03-09 18:48:07 +0100

    Simplified scanning

--- future-maintenance-core/src/main/java/guru/thomasweber/tools/futuremaintenance/core/MaintenanceOccurrence.java
+++ future-maintenance-core/src/main/java/guru/thomasweber/tools/futuremaintenance/core/MaintenanceOccurrence.java
@@ -3,67 +3,34 @@
 // For full license text see: https://github.com/thoweber/future-maintenance/blob/main/LICENSE
 package guru.thomasweber.tools.futuremaintenance.core;
 
+import static java.util.Objects.isNull;
+import static java.util.Objects.requireNonNull;
+
 import guru.thomasweber.tools.futuremaintenance.api.MaintenanceTask;
-import lombok.Getter;
+import java.time.LocalDate;
+import java.util.Optional;
+
+import lombok.*;
 import lombok.experimental.Accessors;
 import org.jspecify.annotations.Nullable;
 
-import static java.util.Objects.nonNull;
-import static java.util.Objects.requireNonNull;
-
 @Accessors(fluent = true)
 @Getter
-public class MaintenanceOccurrence<T extends Enum<T> & MaintenanceTask> {
+@Builder
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@EqualsAndHashCode
+@ToString
+public class MaintenanceOccurrence {
 
-  private final Enum<T> key;
+  private final Key key;
+  @Getter(AccessLevel.NONE)
   private final @Nullable String extraInformation;
-  private @Nullable OccurenceType occurenceType;
-  private @Nullable String className;
-  private @Nullable String locationName;
+  private final OccurenceType occurenceType;
+  private final String className;
+  private final String locationName;
 
-  public MaintenanceOccurrence(Enum<T> t, @Nullable String extraInformation) {
-    this.key = t;
-    this.extraInformation = extraInformation;
-  }
-
-  public static <T extends Enum<T> & MaintenanceTask> MaintenanceOccurrence<T> of(Enum<T> t, @Nullable String extraInformation) {
-    return new MaintenanceOccurrence<>(t, extraInformation);
-  }
-
-  public void withFieldUsage(String className, String fieldLocation) {
-    if (nonNull(occurenceType)) {
-      throw new IllegalStateException("MaintenanceOccurrence is already associated with a type");
-    }
-    this.className = className;
-    this.locationName = fieldLocation;
-    this.occurenceType = OccurenceType.FIELD;
-  }
-
-  public void withMethodUsage(String className, String methodLocation) {
-    if (nonNull(occurenceType)) {
-      throw new IllegalStateException("MaintenanceOccurrence is already associated with a type");
-    }
-    this.className = className;
-    this.locationName = methodLocation;
-    this.occurenceType = OccurenceType.METHOD;
-  }
-
-  public void withClassUsage(String className, String classLocation) {
-    if (nonNull(occurenceType)) {
-      throw new IllegalStateException("MaintenanceOccurrence is already associated with a type");
-    }
-    this.className = className;
-    this.locationName = classLocation;
-    this.occurenceType = OccurenceType.CLASS;
-  }
-
-  public void withConstructorUsage(String className, String constructorLocation) {
-    if (nonNull(occurenceType)) {
-      throw new IllegalStateException("MaintenanceOccurrence is already associated with a type");
-    }
-    this.className = className;
-    this.locationName = constructorLocation;
-    this.occurenceType = OccurenceType.CONSTRUCTOR;
+  public Optional<String> extraInformation() {
+    return Optional.ofNullable(extraInformation);
   }
 
   public OccurenceType occurenceType() {
@@ -79,5 +46,72 @@ public class MaintenanceOccurrence<T extends Enum<T> & MaintenanceTask> {
   public String locationName() {
     requireNonNull(locationName, "locationName must be set before calling this method");
     return locationName;
+  }
+
+  public static class MaintenanceOccurrenceBuilder {
+
+    public MaintenanceOccurrenceBuilder extraInformation(@Nullable String extraInformation) {
+      if (isNull(extraInformation) || extraInformation.isBlank()) {
+        this.extraInformation = null;
+      } else {
+        this.extraInformation = extraInformation;
+      }
+      return this;
+    }
+
+    public MaintenanceOccurrence forClassUsage(String className, String classLocation) {
+      this.className = className;
+      this.locationName = classLocation;
+      this.occurenceType = OccurenceType.CLASS;
+      return build();
+    }
+
+    public MaintenanceOccurrence forFieldUsage(String className, String fieldLocation) {
+      this.className = className;
+      this.locationName = fieldLocation;
+      this.occurenceType = OccurenceType.FIELD;
+      return build();
+    }
+
+    public MaintenanceOccurrence forConstructorUsage(String className, String constructorLocation) {
+      this.className = className;
+      this.locationName = constructorLocation;
+      this.occurenceType = OccurenceType.CONSTRUCTOR;
+      return build();
+    }
+
+    public MaintenanceOccurrence forMethodUsage(String className, String methodLocation) {
+      this.className = className;
+      this.locationName = methodLocation;
+      this.occurenceType = OccurenceType.METHOD;
+      return build();
+    }
+
+    private MaintenanceOccurrence build() {
+      return new MaintenanceOccurrence(this.key, this.extraInformation, this.occurenceType, this.className, this.locationName);
+    }
+  }
+
+  @Accessors(fluent = true)
+  @Getter
+  @EqualsAndHashCode
+  @ToString
+  public static final class Key implements MaintenanceTask {
+
+    private final Class<? extends MaintenanceTask> clazz;
+    private final String issueNumber;
+    private final LocalDate executableAfter;
+    private final String reason;
+
+    private Key(MaintenanceTask task) {
+      this.clazz = task.getClass();
+      this.issueNumber = task.issueNumber();
+      this.executableAfter = task.executableAfter();
+      this.reason = task.reason();
+    }
+
+    public static <T extends Enum<T> & MaintenanceTask> Key of(Enum<T> task) {
+      return new Key((MaintenanceTask) task);
+    }
   }
 }
